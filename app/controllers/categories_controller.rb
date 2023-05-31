04f530192110a06a1c0717298e41dc4e51921f64
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-31 18:59:10 +0630

    add categroy new view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,11 +6,25 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(categories_params)
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      redirect_to new_category_path, alert: 'Category was not created.'
+      puts @category.errors.full_messages
+    end
   end
 
   def splash
+  end
+
+  private
+
+  def categories_params
+    params.require(:category).permit(:name, :icon).merge(author_id: current_user.id)
   end
 end
