474ea2ac8d1b1895f5838d9b20a874c8f06ece54
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-01 22:22:41 +0630

    add style for purchase index page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,10 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.assets.debug = true
+  
+  config.assets.compile = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
