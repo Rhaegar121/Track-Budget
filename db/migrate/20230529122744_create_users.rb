2a0dfa68d989693da8cc5d1cf81cfe048b5e521c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-02 20:26:00 +0630

    fix duplicate user table error in deployment

--- db/migrate/20230529122744_create_users.rb
+++ db/migrate/20230529122744_create_users.rb
@@ -1,9 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    drop_table :users if table_exists?(:users)
+
     create_table :users do |t|
       t.string :name
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :users
   end
 end
