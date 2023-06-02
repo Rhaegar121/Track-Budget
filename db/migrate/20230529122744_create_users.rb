d84c9450933c04c150447824b14ae9d00097f017
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-02 20:42:41 +0630

    fix rubocop errrors

--- db/migrate/20230529122744_create_users.rb
+++ db/migrate/20230529122744_create_users.rb
@@ -1,15 +1,25 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
-  def up
-    drop_table :users if table_exists?(:users)
+  def change
+    # Drop dependent objects first
+    remove_foreign_key :categories, column: :author_id
+    remove_foreign_key :purchases, column: :author_id
+    remove_foreign_key :category_purchases, :categories
+    remove_foreign_key :category_purchases, :purchases
 
+    # Drop the users table
+    drop_table :users
+
+    # Recreate the users table with updated columns and constraints
     create_table :users do |t|
       t.string :name
 
       t.timestamps
     end
-  end
 
-  def down
-    drop_table :users
+    # Add back the foreign key constraints if needed
+    add_foreign_key :categories, :users, column: :author_id
+    add_foreign_key :purchases, :users, column: :author_id
+    add_foreign_key :category_purchases, :categories
+    add_foreign_key :category_purchases, :purchases
   end
 end
