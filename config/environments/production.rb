e3051b3f4a0b18980ff05a1389aadaad0d5a77f2
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-02 15:13:55 +0630

    update production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.default_url_options = { host: 'https://track-budget.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'https://track-budget.onrender.com' }
 end
