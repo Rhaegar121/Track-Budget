de33694855288c6ca03c1575d31c068146773467
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-02 15:15:25 +0630

    remove confirmable from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable,
-  :confirmable
+  :recoverable, :rememberable, :validatable
 
     has_many :categories, dependent: :destroy
     has_many :purchases, dependent: :destroy
