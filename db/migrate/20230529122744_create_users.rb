e97384673aae2ed63b6b96bdbebfd3b3de597414
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-02 22:34:23 +0630

    fix rubocop errors

--- db/migrate/20230529122744_create_users.rb
+++ db/migrate/20230529122744_create_users.rb
@@ -1,9 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    # Drop the users table
-    drop_table :users if table_exists?(:users)
-
-    # Recreate the users table with updated columns and constraints
     create_table :users do |t|
       t.string :name
 
