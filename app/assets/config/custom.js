3f5721654c417805497eb34b2bbef9a23e080729
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-11 23:15:19 +0630

    add popup window for logout confirmation

--- app/assets/config/custom.js
+++ app/assets/config/custom.js
@@ -1,16 +1,31 @@
 document.addEventListener('turbo:load', function() {
-    //side menu
-    const hamburger = document.querySelector('.hamburger');
-    const mobileMenu = document.querySelector('.sideMenu');
-    const main = document.querySelector('.main');
-    
+  //side menu
+  const hamburger = document.querySelector('.hamburger');
+  const mobileMenu = document.querySelector('.sideMenu');
+  const main = document.querySelector('.main');
+  
+  if(hamburger) {
     hamburger.onclick = () => {
       hamburger.classList.toggle('open');
       main.classList.toggle('transform');
       mobileMenu.classList.toggle('width');
     };
+  }
 
-    //popup window
-    const logout = document.querySelector('.fa-right-from-bracket');
-    const popup = document.querySelector('.popup');
+  //popup window
+  const logout = document.querySelector('.fa-right-from-bracket');
+  const popup = document.querySelector('.popup');
+  const cancel = document.querySelector('.cancel');
+
+  if (logout) {
+    logout.onclick = () => {
+      popup.classList.toggle('show');
+    }
+  }
+
+  if (cancel) {
+    cancel.onclick = () => {
+      popup.classList.toggle('show');
+    }
+  }
 });
